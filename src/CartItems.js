a5fc88e0b0fc600ceec7038c608593eb3d0da7fc
Jan Behnke <jan.behnke@gmx.net>
2021-03-16 14:38:20 +0100

    Added Total Price

--- src/CartItems.js
+++ src/CartItems.js
@@ -3,27 +3,33 @@ import CartItem from './CartItem'
 
 class CartItems extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { cartList: props.cartList };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { cartList: props.cartList };
+  }
 
-    render() {
-        return <div className="container">
-        <h1>Cart Items</h1>
-        <div className="list-group">
-          <div className="list-group-item">
-            <div className="row">
-              <div className="col-md-8">Product</div>
-              <div className="col-md-2">Price</div>
-              <div className="col-md-2">Quantity</div>
-            </div>
+  calculateTotal() {
+    let total = this.state.cartList.reduce((acc, val) => { return acc = acc + (val.product.priceInCents / 100) }, 0);
+    return total;
+  }
+
+  render() {
+    return <div className="container">
+      <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
           </div>
-          { this.state.cartList.map(listitem => <CartItem name={listitem.product.name} price={listitem.product.priceInCents} quantity={listitem.quantity}/>) }
         </div>
+        {this.state.cartList.map(listitem => <CartItem id={listitem.id} name={listitem.product.name} price={listitem.product.priceInCents} quantity={listitem.quantity} />)}
+        <div>Total Price: $ {this.calculateTotal()}</div>
       </div>
-      
-    }
+    </div>
+
+  }
 }
 
 export default CartItems;
